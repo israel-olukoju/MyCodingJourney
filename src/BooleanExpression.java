92eea7e0014acc5864708cd9d81a1d37272283b0
MyJava <issytemmy2019@gmail.com>
2025-09-15 21:11:56 +0100

    initial commit for this project

---
+++ src/BooleanExpression.java
@@ -0,0 +1,10 @@
+public class BooleanExpression {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(x > 0);
+        System.out.println(x < 0);
+        System.out.println(x != 0);
+        System.out.println(x >= 0);
+        System.out.println(x != 1);
+    }
+}
