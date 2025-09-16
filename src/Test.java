2b79edaf277c72bc95d9aef9e834e5fa1160975d
MyJava <issytemmy2019@gmail.com>
2025-09-16 19:57:38 +0100

    initial commit for this project

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        boolean even = true;
+        if (even) {
+            System.out.println("True");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
