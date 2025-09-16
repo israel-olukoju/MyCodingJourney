1431a3bb93b3631a730812ef757263941bef0a35
MyJava <issytemmy2019@gmail.com>
2025-09-16 18:40:31 +0100

    initial commit for this project

--- src/IfXY.java
+++ src/IfXY.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 
 public class IfXY {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        // Prompt user for x and y
+        System.out.print("Enter a number for x (integers): ");
+        int x = input.nextInt();
+        System.out.print("Enter a number for y (integers): ");
+        int y = input.nextInt();
+        // X+Y
+        int z = x + y;
+        if (x > 2) {
+            if (y > 2) {
+                System.out.println(x + " + " + y + " = " + z);
+            }
+        } else {
+            System.out.println("x is " + x);
+        }
     }
 }
