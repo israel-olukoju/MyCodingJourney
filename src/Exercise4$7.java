e5259457902c2c45c890bc0618fc28282e315b4e
MyJava <issytemmy2019@gmail.com>
2025-10-23 22:43:15 +0100

    initial commit for this project

--- src/Exercise4$7.java
+++ src/Exercise4$7.java
@@ -1,5 +1,6 @@
 public class Exercise4$7 {
     public static void main(String[] args) {
+
         System.out.println("What is Math.log(Math.exp(5.5))? " + Math.log(Math.exp(5.5)));
         System.out.println("What is Math.exp(Math.log(5.5))? " + Math.exp(Math.log(5.5)));
         System.out.println("What is Math.asin(Math.sin(Math.PI / 6))? " + Math.asin(Math.sin(Math.PI/ 6)));
